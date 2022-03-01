5d51a35c9c19d524ff3e3b4d4deea9ff9facdffd
Johannes Birkeland <johannesbi@uia.no>
2022-03-01 07:17:51 +0000

    #45 Python script med innlastning av geojson og sammenligning.
    - Funksjon for å laste geojson inn som en liste med Observasjon objekter
    - For hver observasjon så ses det på forskjeller i areal med tidligere observasjoner.
    - Funksjon fo r å få IOU mellom observasjoner. Union funksjonen fra turfpy bruker den deprecated funksjonen cascaded_union() fra shapely.ops. Men burde bruke unary_union. KSkal implementere en bedre fun
    - Fikset IOU funksjon
    - Oppdaterte filnavn
    
    Related work items: #49, #50

---
+++ src/eksperiment_johannes/helper_functions.py
@@ -0,0 +1,66 @@
+from typing import List, Union
+
+from geojson import Feature, FeatureCollection
+
+from shapely.geometry import mapping, shape
+from shapely.ops import unary_union
+
+from turfpy.helper import get_geom
+
+
+def union(
+        features: Union[List[Feature], FeatureCollection]
+) -> Union[Feature, FeatureCollection]:
+    shapes = []
+    properties_list = []
+    if isinstance(features, list):
+        for f in features:
+            if f.type != "Feature":
+                raise Exception("Not a valid feature")
+            geom = get_geom(f)
+            s = shape(geom)
+            shapes.append(s)
+
+            if "properties" in f.keys():
+                properties_list.append(f["properties"])
+    else:
+        if "features" not in features.keys():
+            raise Exception("Invalid FeatureCollection")
+        if "properties" in features.keys():
+            properties_list.append(features["properties"])
+
+        for f in features["features"]:
+            geom = get_geom(f)
+            s = shape(geom)
+            shapes.append(s)
+
+            if "properties" in f.keys():
+                properties_list.append(f["properties"])
+
+    result = unary_union(shapes)
+    result = mapping(result)
+    properties = merge_dict(properties_list)
+
+    if result["type"] == "GeometryCollection":
+        features = []
+        for geom in result["geometries"]:
+            features.append(Feature(geometry=geom))
+        return FeatureCollection(features, properties=properties)
+
+    return Feature(geometry=result, properties=properties)
+
+
+def merge_dict(dicts: list):
+    super_dict: dict = {}
+    for d in dicts:
+        for k, v in d.items():
+            if k not in super_dict.keys():
+                super_dict[k] = v
+            else:
+                if isinstance(super_dict[k], list):
+                    if v not in super_dict[k]:
+                        super_dict[k].append(v)
+                else:
+                    if super_dict[k] != v:
+                        super_dict[k] = [super_dict[k], v]
+    return super_dict
---
+++ src/eksperiment_johannes/similarity_analysis.py
@@ -0,0 +1,103 @@
+import json
+from geojson import FeatureCollection
+from shapely.geometry import Polygon, Point
+from area import area
+from turfpy.transformation import intersect
+from helper_functions import union
+
+
+class Observation:
+    def __init__(self, date, dataset, geo_type, coordinates, obs_area, geojson):
+        self.date = date
+        self.dataset = dataset
+        self.geo_type = geo_type
+        self.coordinates = coordinates
+        self.area = obs_area
+        self.geojson = geojson
+
+    def __repr__(self):
+        return '{' + self.date + ', ' + self.dataset + ', ' + str(self.coordinates) + "}"
+
+
+# Returns a list of Observation objects based on the dictionary of geojson data input.
+def load_observations_from_file(jsonfile) -> list:
+    loaded_data = json.load(open(jsonfile, "r"))
+    list_of_observations = []
+    for key in loaded_data['features']:
+        properties = key['properties']
+        geometry = key['geometry']
+        coordinates = geometry['coordinates'][0] if (len(geometry['coordinates']) == 1) else geometry['coordinates']
+        geojson_area = round(area(geometry), 4)
+        observation = Observation(
+            properties['date'],
+            properties['dataset'],
+            geometry['type'],
+            coordinates,
+            geojson_area,
+            key)
+
+        list_of_observations.append(observation)
+    return list_of_observations
+
+
+def measure_iou_of_observations(observation_1: Observation, observation_2: Observation) -> float:
+    coordinates_1 = observation_1.geojson['geometry']
+    coordinates_2 = observation_2.geojson['geometry']
+    intersection = intersect([coordinates_1, coordinates_2])
+    union_ = union(FeatureCollection([coordinates_1, coordinates_2], properties={"combine": "yes"}))
+    area_of_intersection = area(intersection['geometry'])
+    area_of_union = area(union_['geometry'])
+    return area_of_intersection / area_of_union
+
+
+def difference_in_area(area1: float, area2: float) -> float:
+    return round(area1 - area2, 4)
+
+
+if __name__ == "__main__":
+    data = load_observations_from_file("../../resources/observasjoner_2021.json") \
+           + load_observations_from_file("../../resources/observasjoner_2020.json") \
+           + load_observations_from_file("../../resources/observasjoner_2022.json")
+
+    # Sorterer alle observasjon objekter etter dato
+    data.sort(key=lambda obs: obs.date)
+
+    for i in range(len(data)):
+        curr_observation = data[i]
+        print(
+            curr_observation.date + " - " + curr_observation.dataset + " - " + curr_observation.geo_type + " - " + str(
+                curr_observation.area) + "m²")
+        if i == 0:  # Første observasjon ignoreres.
+            print("|     First Observation")
+        else:  # Hver observasjon sammenlignes med tidligere observasjoner. Både flater og punkt.
+            for j in range(i, 0, -1):
+                last_observation = data[j - 1]
+                if len(curr_observation.coordinates) > 2:
+                    if len(last_observation.coordinates) > 2:
+                        difference_in_area_ = difference_in_area(curr_observation.area, last_observation.area)
+                        iou_of_observations = measure_iou_of_observations(curr_observation, last_observation)
+                        print(
+                            "|    Difference in area: " + last_observation.date + " - " + last_observation.dataset
+                            + ": " + str(difference_in_area_) + "m²"
+                        )
+                        print(
+                            "|    IOU of current observation and " + last_observation.date + " - "
+                            + last_observation.dataset + ": " + str(iou_of_observations))
+                        print("____________________________________________________________________")
+                    else:
+                        point = Point(last_observation.coordinates)
+                        polygon = Polygon(curr_observation.coordinates)
+                        print(
+                            "|    " + last_observation.date + " - " + last_observation.dataset + " exists in between this observation's coordinates: " + str(
+                                polygon.contains(point)))
+                elif len(curr_observation.coordinates) == 2:
+                    point1 = Point(curr_observation.coordinates)
+                    point_or_polygon = Polygon(last_observation.coordinates) if len(last_observation.coordinates) > 2 \
+                        else Point(last_observation.coordinates)
+                    print(
+                        "|    Point is present in  - " + last_observation.date + " - " + last_observation.dataset + ": "
+                        + str(point_or_polygon.contains(point1))
+                        + "\n____________________________________________________________________"
+                    )
+
+        print("\n\n")
